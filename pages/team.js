e43404c0ca4defbad4e5ab90447c083db793ed75
elvin-osmanov <elvinfo@code.edu.az>
2022-06-27 18:21:20 +0100

    making changes for mobile view

--- pages/team.js
+++ pages/team.js
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import Back from "../components/Back";
 import { API } from "../config/api";
@@ -19,7 +19,16 @@ const Team = ({ excerpt, team }) => {
   const instagram = "https://instagram.com";
   const facebook = "https://facebook.com";
   const linkedin = "https://www.linkedin.com/in";
+  const [mobileView, setMobileView] = useState(false);
+  useEffect(() => {
+    if (window.screen.availWidth < 500) {
+      setMobileView(true);
+    } else {
+      setMobileView(false);
+    }
+  }, [mobileView]);
 
+  console.log(mobileView);
   return (
     <Layout
       title="Meet The Team"
@@ -32,36 +41,37 @@ const Team = ({ excerpt, team }) => {
           {parse(excerpt)}
         </h2>
 
-        <section className="grid grid-cols-1  gap-y-16  mt-10">
-          {team?.length > 0 &&
-            team.map((staff, index) => (
-              <motion.div
-                key={index}
-                initial={{ opacity: 0, y: 100 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                viewport={{ once: true }}
-                className="w-full flex flex-col gap-2 bg-white rounded-xl drop-shadow-md"
-              >
-                <div className="flex items-center space-x-10 px-3 lg:px-7 py-5 ">
-                  <div className="w-[35%]">
-                    <img
-                      src={
-                        staff?.attributes?.image?.data?.attributes?.formats
-                          ?.small?.url ||
-                        staff?.attributes?.image?.data?.attributes?.formats
-                          ?.medium?.url ||
-                        staff?.attributes?.image?.data?.attributes?.formats
-                          ?.large?.url ||
-                        staff?.attributes?.image?.data?.attributes?.formats
-                          ?.thumbnail?.url ||
-                        "Placeholder.png"
-                      }
-                      alt={staff?.attributes?.name || "Staff"}
-                      className="w-full h-full aspect-square rounded-xl object-cover"
-                    />
-                  </div>
-                  <div className="w-[65%]">
-                    <div className="flex flex-col ">
+        {mobileView ? (
+          <section className="grid grid-cols-1  gap-y-14  mt-10">
+            {team?.length > 0 &&
+              team.map((staff, index) => (
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, y: 100 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  viewport={{ once: true }}
+                  className="w-full flex flex-col gap-2 bg-white rounded-xl drop-shadow-md"
+                >
+                  <div className="flex items-center space-x-10 px-3 lg:px-7 py-5 ">
+                    <div className="w-[50%] ">
+                      <img
+                        src={
+                          staff?.attributes?.image?.data?.attributes?.formats
+                            ?.small?.url ||
+                          staff?.attributes?.image?.data?.attributes?.formats
+                            ?.medium?.url ||
+                          staff?.attributes?.image?.data?.attributes?.formats
+                            ?.large?.url ||
+                          staff?.attributes?.image?.data?.attributes?.formats
+                            ?.thumbnail?.url ||
+                          "Placeholder.png"
+                        }
+                        alt={staff?.attributes?.name || "Staff"}
+                        className="w-full h-full aspect-square rounded-xl object-cover"
+                      />
+                    </div>
+
+                    <div className="flex flex-col space-y-2">
                       <h1 className="text-[28px] lg:text-5xl text-primary font-[900] leading-tight">
                         {staff?.attributes?.name || "Staff"}
                       </h1>
@@ -71,29 +81,27 @@ const Team = ({ excerpt, team }) => {
                       <p className="text-sm lg:text-xl font-semibold poppins">
                         {staff?.attributes?.position}
                       </p>
-                      <div className="  bg-neutral-400 bg-opacity-[0.03] text-neutral-800 font-medium poppins text-sm lg:text-lg leading-relaxed px-4  lg:px-0 py-5">
-                        {parse(staff?.attributes?.bio)}
-                      </div>
+
                       <div className="flex gap-2">
                         {staff?.attributes?.twitter && (
                           <Icon
                             url={`${twitter}/${staff?.attributes?.twitter}`}
                           >
-                            <FaTwitter size={20} />
+                            <FaTwitter size={12} />
                           </Icon>
                         )}
                         {staff?.attributes?.linkedin && (
                           <Icon
                             url={`${linkedin}/${staff?.attributes?.linkedin}`}
                           >
-                            <FaLinkedinIn size={20} />
+                            <FaLinkedinIn size={12} />
                           </Icon>
                         )}
                         {staff?.attributes?.facebook && (
                           <Icon
                             url={`${facebook}/${staff?.attributes?.facebook}`}
                           >
-                            <FaFacebookF size={20} />
+                            <FaFacebookF size={12} />
                           </Icon>
                         )}
 
@@ -101,16 +109,99 @@ const Team = ({ excerpt, team }) => {
                           <Icon
                             url={`${instagram}/${staff?.attributes?.instagram}`}
                           >
-                            <FaInstagram size={20} />
+                            <FaInstagram size={12} />
                           </Icon>
                         )}
                       </div>
                     </div>
                   </div>
-                </div>
-              </motion.div>
-            ))}
-        </section>
+                  <div className=" bg-neutral-400 bg-opacity-[0.03] text-neutral-800 font-medium poppins text-sm lg:text-lg leading-relaxed px-4  lg:px-0 py-5">
+                    {parse(staff?.attributes?.bio)}
+                  </div>
+                </motion.div>
+              ))}
+          </section>
+        ) : (
+          <section className="grid grid-cols-1  gap-y-16  mt-10">
+            {team?.length > 0 &&
+              team.map((staff, index) => (
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, y: 100 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  viewport={{ once: true }}
+                  className="w-full flex flex-col gap-2 bg-white rounded-xl drop-shadow-md"
+                >
+                  <div className="flex items-center space-x-10 px-3 lg:px-7 py-5 ">
+                    <div className="w-[35%]">
+                      <img
+                        src={
+                          staff?.attributes?.image?.data?.attributes?.formats
+                            ?.small?.url ||
+                          staff?.attributes?.image?.data?.attributes?.formats
+                            ?.medium?.url ||
+                          staff?.attributes?.image?.data?.attributes?.formats
+                            ?.large?.url ||
+                          staff?.attributes?.image?.data?.attributes?.formats
+                            ?.thumbnail?.url ||
+                          "Placeholder.png"
+                        }
+                        alt={staff?.attributes?.name || "Staff"}
+                        className="w-full h-full aspect-square rounded-xl object-cover"
+                      />
+                    </div>
+                    <div className="w-[65%]">
+                      <div className="flex flex-col ">
+                        <h1 className="text-[28px] lg:text-5xl text-primary font-[900] leading-tight">
+                          {staff?.attributes?.name || "Staff"}
+                        </h1>
+                        <p className="text-xs lg:text-xl font-light poppins">
+                          {staff?.attributes?.pronouns}
+                        </p>
+                        <p className="text-sm lg:text-xl font-semibold poppins">
+                          {staff?.attributes?.position}
+                        </p>
+                        <div className=" bg-neutral-400 bg-opacity-[0.03] text-neutral-800 font-medium poppins text-sm lg:text-lg leading-relaxed px-4  lg:px-0 py-5">
+                          {parse(staff?.attributes?.bio)}
+                        </div>
+                        <div className="flex gap-2">
+                          {staff?.attributes?.twitter && (
+                            <Icon
+                              url={`${twitter}/${staff?.attributes?.twitter}`}
+                            >
+                              <FaTwitter size={20} />
+                            </Icon>
+                          )}
+                          {staff?.attributes?.linkedin && (
+                            <Icon
+                              url={`${linkedin}/${staff?.attributes?.linkedin}`}
+                            >
+                              <FaLinkedinIn size={20} />
+                            </Icon>
+                          )}
+                          {staff?.attributes?.facebook && (
+                            <Icon
+                              url={`${facebook}/${staff?.attributes?.facebook}`}
+                            >
+                              <FaFacebookF size={20} />
+                            </Icon>
+                          )}
+
+                          {staff?.attributes?.instagram && (
+                            <Icon
+                              url={`${instagram}/${staff?.attributes?.instagram}`}
+                            >
+                              <FaInstagram size={20} />
+                            </Icon>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </motion.div>
+              ))}
+          </section>
+        )}
       </main>
     </Layout>
   );
