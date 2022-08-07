905b1c0d5b403b06bb40337a5846af47241f6335
sam-20 <botwesamuel192@gmail.com>
2022-08-08 00:15:52 +0100

    added au autoadsense auto ads

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import Router from "next/router";
 import nProgress from "nprogress";
 import { ChakraProvider } from "@chakra-ui/react";
 import { GlobalProvider } from "../context/GlobalContext";
+import Script from "next/script";
 
 //Router events
 Router.events.on("routeChangeStart", nProgress.start);
@@ -16,6 +17,16 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <GlobalProvider>
+        <Script
+          id="Adsense-id"
+          async
+          onError={(e) => {
+            console.error("Script failed to load", e);
+          }}
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9166716457553506"
+          crossorigin="anonymous"
+        ></Script>
         <Component {...pageProps} />
       </GlobalProvider>
     </ChakraProvider>
