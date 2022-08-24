77b25fc4c51b9a9f45f6ccfd99e4639cf8cbac01
ghost <ghostalabi95@gmail.com>
2022-08-24 14:43:15 +0100

    charset syntax error bugfix

--- defaults/Layout.jsx
+++ defaults/Layout.jsx
@@ -100,7 +100,7 @@ const Layout = ({
           `,
           }}
         />
-        <meta charset="UTF-8"/>
+        <meta charSet="UTF-8"/>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, user-scalable=no"
