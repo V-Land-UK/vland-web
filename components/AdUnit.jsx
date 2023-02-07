e5741e4a29f9112111fb7051875fd0cbcdf7d9ec
sam-20 <botwesamuel192@gmail.com>
2023-02-07 19:45:07 +0000

    ad setup update

--- components/AdUnit.jsx
+++ components/AdUnit.jsx
@@ -13,11 +13,12 @@ function AdUnit() {
     <div>
       <ins
         //className="adsbygoogle block mx-auto w-[300px]  lg:w-[728px] h-[50px] lg:h-[90px]"
-        className=" block mx-auto w-[94%]  md:h-[90px] sm:h-[60px] xxs:h-[60px] lg:h-[90px]"
+        // className="adsbygoogle block mx-auto w-[94%]  md:h-[90px] sm:h-[60px] xxs:h-[60px] lg:h-[90px]"
+        className="adsbygoogle"
         style={{ display: "block" }}
         data-ad-client="ca-pub-9166716457553506"
         data-ad-slot="6593416244"
-        data-ad-format="rectangle horizontal"
+        data-ad-format="auto"
         data-full-width-responsive="true"
       ></ins>
     </div>
